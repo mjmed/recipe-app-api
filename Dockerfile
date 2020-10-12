9de0c5f0d0bd7fd7ae28052b9424ea65e79fb5be
Maria Jose Medina <mjmedina.c719@gmail.com>
2020-10-12 15:18:33 -0300

    Configured postgres db.

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,14 @@ MAINTAINER Maria_Jose
 ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /requirements.txt
+# Instala el cliente de Postgres
+RUN apk add --update --no-cache postgresql-client
+# dependencias temporales
+RUN apk add --update --no-cache --virtual .tmp-build-deps \
+        gcc libc-dev linux-headers postgresql-dev
 RUN pip install -r /requirements.txt
+# elimina los requisitos temporales
+RUN apk del .tmp-build-deps
 
 RUN mkdir /app
 WORKDIR /app
---
+++ app/core/tests/test_commands.py
@@ -0,0 +1,30 @@
+# Permite burlarse (hacer mocking) del comportamiento de la función get_database de Django.
+# Simulará que la base de datos esta disponible o no mientras se prueba el comando.
+from unittest.mock import patch
+
+from django.core.management import call_command
+
+# Error operacional que Django arroja cuando la BD no está disponible.
+# Se utilizará para simular que la BD está disponible o no cuando se ejecute el comando.
+from django.db.utils import OperationalError
+
+from django.test import TestCase
+
+
+
+class CommandTests(TestCase):
+
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.return_value = True
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 1)
+
+    @patch('time.sleep', return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch('django.db.utils.ConnectionHandler.__getitem__') as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command('wait_for_db')
+            self.assertEqual(gi.call_count, 6)
